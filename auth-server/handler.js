8b3bed2a78267c0ccae4c20ac83ed0b649e39424
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-12-21 08:48:23 -0500

    added function getAccessToken

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -14,10 +14,10 @@ const credentials = {
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   redirect_uris: ["https://farmeroy.github.io/meet-app/"],
-  javascript_origins: ["https://farmeroy.github.io", "http://localhost:3000"],
+  javascript_origins: ["https://farmeroy.github.io", "http://localhost:8080"],
 };
 
-const {client_secret, client_id, redirect_uris, calendar_id } = credentials;
+const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
 
 const oAuth2Client = new google.auth.OAuth2(
   client_id,
@@ -42,4 +42,40 @@ module.exports.getAuthURL = async () => {
       authUrl: authUrl,
     }),
   };
+};
+
+module.exports.getAccessToken = async (event) => {
+  // every serverless function needs a new oAuth2Client, there is no persistent data
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+  return new Promise((resolve, reject) => {
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(token);
+    });
+  })
+    .then((token) => {
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify(token),
+      };
+    })
+    .catch((err) => {
+      console.error(err);
+      return {
+        statusCode: 500,
+        body: JSON.stringify(err),
+      };
+    });
 };
