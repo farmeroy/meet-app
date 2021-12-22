b34531c2da8f12687508aabc0f4dace872ef3440
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-12-22 07:30:21 -0500

    set allow credential to true

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -115,6 +115,7 @@ module.exports.getCalendarEvents = async (event) => {
         statusCode: 200,
         headers: {
           "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
         },
         body: JSON.stringify({ events: results.data.items }),
       };
