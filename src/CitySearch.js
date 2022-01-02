8696ec8962d40558f29a995d19cee1133f966236
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-02 10:50:41 -0500

    added query state and inputChangeHandler

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 
 class CitySearch extends Component {
+  state = {
+    query: "",
+  };
+
+  inputChangeHandler = (event) => {
+    const value = event.target.value;
+    this.setState({ query: value });
+  };
+
   render() {
     return (
       <div className="CitySearch">
-        <input type="text" className="city" />
-        <ul className="suggestions">
-          </ul>
+        <input
+          type="text"
+          className="city"
+          value={this.state.query}
+          onChange={this.inputChangeHandler}
+        />
+        <ul className="suggestions"></ul>
       </div>
     );
   }
