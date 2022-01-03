d79cb105a5a8061162ec525855b56a2d965f67af
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-03 09:11:08 -0500

    added suggestions state, map suggestsions to li elements, and filter suggestions by query

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -3,11 +3,15 @@ import React, { Component } from "react";
 class CitySearch extends Component {
   state = {
     query: "",
+    suggestions: [],
   };
 
   inputChangeHandler = (event) => {
     const value = event.target.value;
-    this.setState({ query: value });
+    const suggestions = this.props.locations.filter((location) => {
+      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+    });
+    this.setState({ query: value, suggestions, });
   };
 
   render() {
@@ -19,7 +23,14 @@ class CitySearch extends Component {
           value={this.state.query}
           onChange={this.inputChangeHandler}
         />
-        <ul className="suggestions"></ul>
+        <ul className="suggestions">
+          {this.state.suggestions.map((suggestion) => (
+            <li key={suggestion}>{suggestion}</li>
+          ))}
+          <li key="all">
+            <b>See all cities</b>
+          </li>
+        </ul>
       </div>
     );
   }
