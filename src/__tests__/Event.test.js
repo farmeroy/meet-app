9925fce6ff7c816ca11bff6d2088c9287a360eb8
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-03 11:23:49 -0500

    first commit, includes test for find summary element and check the text is from the mockdata

---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import Event from '../Event';
+import { mockData } from '../mock-data';
+
+describe("<Event /> component", () => {
+  let EventWrapper;
+  beforeAll(() => {
+    EventWrapper = shallow(<Event event={mockData[0]}/>);
+  });
+  
+  test("render event title", () => {
+    expect(EventWrapper.find(".summary")).toHaveLength(1);
+  });
+
+  test("renders summary text correctly", () => {
+      expect(EventWrapper.find('.summary').text()).toBe(mockData[0].summary)
+  })
+
+});
