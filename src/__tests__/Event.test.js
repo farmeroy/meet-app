6d751373f33b817904a0a97072e80b431a955b02
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-03 13:32:48 -0500

    show event details if no collapsed test, prettified

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -1,29 +1,38 @@
-import React from 'react';
-import {shallow} from 'enzyme';
-import Event from '../Event';
-import { mockData } from '../mock-data';
+import React from "react";
+import { shallow } from "enzyme";
+import Event from "../Event";
+import { mockData } from "../mock-data";
 
 describe("<Event /> component", () => {
   let EventWrapper;
   beforeAll(() => {
-    EventWrapper = shallow(<Event eventData={mockData[0]}/>);
+    EventWrapper = shallow(<Event eventData={mockData[0]} />);
   });
-  
+
   test("render event title", () => {
     expect(EventWrapper.find(".summary")).toHaveLength(1);
   });
 
   test("renders summary text correctly", () => {
-      expect(EventWrapper.find('.summary').text()).toBe(mockData[0].summary)
+    expect(EventWrapper.find(".summary").text()).toBe(mockData[0].summary);
   });
 
-  test('when component first loads, event state is collapsed', () => {
-    expect(EventWrapper.state('isCollapsed')).toBe(true);
+  test("when component first loads, event state is collapsed", () => {
+    expect(EventWrapper.state("isCollapsed")).toBe(true);
   });
 
-  test('when user clicks on a collapsed event, isCollapsed is set to false',() => {
-    EventWrapper.setState({isCollapsed: true});
-    EventWrapper.find('.Event').simulate('click');
+  test("when user clicks on a collapsed event, isCollapsed is set to false", () => {
+    EventWrapper.setState({ isCollapsed: true });
+    EventWrapper.find(".Event").simulate("click");
     expect(EventWrapper.state("isCollapsed")).toBe(false);
   });
-})
+
+  test("when an event is expanded, show the event details", () => {
+    EventWrapper.setState({ isCollapsed: false });
+    expect(EventWrapper.find(".eventDetails")).toHaveLength(1);
+  });
+
+  test("renders details of event correctly", () => {
+
+  })
+});
