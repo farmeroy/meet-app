1014730475c97a6d8a714bd82a17fc3510e7b2ad
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-03 11:32:29 -0500

    added isCollapsed state

--- src/Event.jsx
+++ src/Event.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
 class Event extends Component {
+
+  state = {
+    isCollapsed: true
+  }
   render() {
     return <div>
       <h1 className='summary'>{this.props.eventData.summary}</h1></div>;
