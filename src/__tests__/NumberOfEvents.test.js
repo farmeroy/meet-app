bd630b6b577f11ade155a01aaaa2e5aee4f7bfb7
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-03 16:33:13 -0500

    added tests, render correctly and default value

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -2,3 +2,18 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import NumberOfEvents from '../NumberOfEvents';
 
+describe('<NumberOfEvents /> component', () => {
+  let NumberOfEventsWrapper;
+  
+  beforeAll(()=>{
+    NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+  });
+
+  test('render number input', () => {
+    expect(NumberOfEventsWrapper.find('.numberInput')).toHaveLength(1);
+  });
+
+  test('default input value is 32', () => {
+    expect(NumberOfEventsWrapper.find('.numberInput').prop('value')).toBe('32');
+  });
+})
