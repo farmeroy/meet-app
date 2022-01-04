8db9abbf6bfc0f210056603f3389b2ed8636cd01
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-04 15:07:08 -0500

    added updateEvents method

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import "./App.css";
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
 import NumberOfEvents from './NumberOfEvents';
+import {getEvents} from './api';
 
 class App extends Component {
 
   state = {
     events: [],
     locations: []
+  }
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents
+      });
+    });
   }
 
   render() {
