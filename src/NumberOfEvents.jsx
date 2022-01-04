01c316311e7eda82252f4fa16dd9ffe558b830f3
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-04 00:29:28 -0500

    input value is derived from state value

--- src/NumberOfEvents.jsx
+++ src/NumberOfEvents.jsx
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
 
 class NumberOfEvents extends Component {
+
+  state = {
+    numberInputValue: 32
+  }
   render() {
-    return <input className="numberInput" type="number" value="32" />;
+    return <input className="numberInput" type="number" value={this.state.numberInputValue} />;
   }
 }
 
