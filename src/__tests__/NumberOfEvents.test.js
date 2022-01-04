c53c7dd851b645033bcac219f2af8730ddbb9bb5
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-04 00:28:47 -0500

    default value is equal to default state test, test change state when input changes

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -13,7 +13,14 @@ describe('<NumberOfEvents /> component', () => {
     expect(NumberOfEventsWrapper.find('.numberInput')).toHaveLength(1);
   });
 
-  test('default input value is 32', () => {
-    expect(NumberOfEventsWrapper.find('.numberInput').prop('value')).toBe('32');
+  test('default input value is the default state', () => {
+    const defaultNumber = NumberOfEventsWrapper.state('numberInputValue');
+    expect(NumberOfEventsWrapper.find('.numberInput').prop('value')).toBe(defaultNumber);
   });
+
+  test('change state when number input changes', () => {
+    NumberOfEventsWrapper.setState({numberInputValue: '32'});
+    NumberOfEventsWrapper.find('.numberInput').simulate('change', {target: {value: "18"}});
+    expect(NumberOfEventsWrapper.state('numberInputValue')).toEqual(18);
+  })
 })
