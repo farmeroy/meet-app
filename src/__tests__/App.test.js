c6a8942097aaa4e7f4f153d726ace92022e37ccc
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-04 09:53:39 -0500

    added full rendering scope and test that app sends events prop to eventList

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from '../App';
-import EventList from '../EventList';
-import CitySearch from '../CitySearch';
-import NumberOfEvents from '../NumberOfEvents';
+import React from "react";
+import { shallow, mount } from "enzyme";
+import App from "../App";
+import EventList from "../EventList";
+import CitySearch from "../CitySearch";
+import NumberOfEvents from "../NumberOfEvents";
 
-describe('<App /> component', () => {
+describe("<App /> component", () => {
   let AppWrapper;
   beforeAll(() => {
     AppWrapper = shallow(<App />);
   });
-  
-  test('render list of events', () => {
+
+  test("render list of events", () => {
     expect(AppWrapper.find(EventList)).toHaveLength(1);
   });
-  
-  test('render CitySearch', () => {
-    expect(AppWrapper.find(CitySearch)).toHaveLength(1);
-  })
 
-  test('render NumberOfEvents', () => {
-    expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
+  test("render CitySearch", () => {
+    expect(AppWrapper.find(CitySearch)).toHaveLength(1);
   });
 
+  test("render NumberOfEvents", () => {
+    expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
+  });
+});
+
+describe("<App /> integration", () => {
+  test('App passes "events" state as a prop to EventList', () => {
+    const AppWrapper = mount(<App />);
+    const AppEventsState = AppWrapper.state("events");
+    expect(AppEventsState).not.toEqual(undefined);
+    expect(AppWrapper.find(EventList).props().events).toEqual(AppEventsState);
+    AppWrapper.unmount();
+  });
 });
