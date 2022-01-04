9617abc2d305436fabe41568ccaf95c3ddd0bd53
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-04 10:17:12 -0500

    added basic getEvents function

--- src/api.js
+++ src/api.js
@@ -1,14 +1,23 @@
 /**
+ *
  * 
  * @param {*} events:
  * The following function should be in the 'api.js' file.
  * This function takes an events array, then uses map to create a new array with only locations.
- * It will also remove all duplicates by creating another new arraw using the spread operator and spreading a Set.
+ * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
  * The Set will remove all duplicates from the array
  */
+
+import { mockData } from './mock-data';
 
 export const extractLocations = (events) => {
   var extractLocations = events.map((event) => event.location);
   var locations = [...new Set(extractLocations)];
   return locations;
+};
+
+
+
+export const getEvents = async () => {
+  return mockData;
 };
