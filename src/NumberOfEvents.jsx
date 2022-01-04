ab09482752831a7c37a3b65955d354167c7849b6
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-04 00:34:07 -0500

    added onChangeHandler to update number state

--- src/NumberOfEvents.jsx
+++ src/NumberOfEvents.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 
 class NumberOfEvents extends Component {
-
   state = {
-    numberInputValue: 32
-  }
+    numberInputValue: 32,
+  };
+
+  onChangeHandler = (event) => {
+    this.setState({
+      numberInputValue: event.target.value,
+    });
+  };
+
   render() {
-    return <input className="numberInput" type="number" value={this.state.numberInputValue} />;
+    return (
+      <input
+        className="numberInput"
+        type="number"
+        value={this.state.numberInputValue}
+        onChange={(event) => this.onChangeHandler(event)}
+      />
+    );
   }
 }
 
