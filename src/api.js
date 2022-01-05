c8037ffac1c22673f1d029bc4e4363072d34c96b
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-05 11:06:04 -0500

    added checkToken function and getAccessToken function

--- src/api.js
+++ src/api.js
@@ -9,6 +9,7 @@
  */
 
 import { mockData } from './mock-data';
+import axios from 'axios';
 
 export const extractLocations = (events) => {
   var extractLocations = events.map((event) => event.location);
@@ -16,8 +17,35 @@ export const extractLocations = (events) => {
   return locations;
 };
 
+const checkToken = async (accessToken) => {
+  const result = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  )
+    .then((res) => res.json())
+    .catch((error) => error.json());
 
+  return result;
+};
 
 export const getEvents = async () => {
   return mockData;
 };
+
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem('access_token');
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem('access_token');
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get('code');
+    if (!code) {
+      const results = await axios.get(
+      "https://fw4pf3ipga.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
+      );
+      const {authURL} = results.data;
+      return (window.location.href = authURL);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
+}
