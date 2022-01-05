e841807d5d5c98d6780603cc07054c45112a7ad6
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-05 08:56:06 -0500

    added check in updateEvents for all events, getEvents when component mounts

--- src/App.js
+++ src/App.js
@@ -2,29 +2,47 @@ import React, { Component } from "react";
 import "./App.css";
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
-import NumberOfEvents from './NumberOfEvents';
-import {getEvents} from './api';
+import NumberOfEvents from "./NumberOfEvents";
+import { getEvents, extractLocations } from "./api";
 
 class App extends Component {
-
   state = {
     events: [],
-    locations: []
+    locations: [],
+  };
+
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
   }
 
   updateEvents = (location) => {
     getEvents().then((events) => {
-      const locationEvents = events.filter((event) => event.location === location);
+      const locationEvents =
+        location === "all"
+          ? events
+          : events.filter((event) => event.location === location);
       this.setState({
-        events: locationEvents
+        events: locationEvents,
       });
     });
-  }
+  };
 
   render() {
     return (
       <div className="App">
-        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
+        />
         <EventList events={this.state.events} />
         <NumberOfEvents />
       </div>
