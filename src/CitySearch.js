4bf904258e4ce3d1089dc29e8b1fd6aa2baaa40b
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-05 08:54:49 -0500

    added itemClickHandler to all cities suggestions

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -39,7 +39,7 @@ class CitySearch extends Component {
               {suggestion}
             </li>
           ))}
-          <li key="all">
+          <li key="all" onClick={() => this.itemClickHandler("all")}>
             <b>See all cities</b>
           </li>
         </ul>
