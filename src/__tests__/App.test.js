c6669ca87ae634bb2a8dc8057ad0a712197c6f9f
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-04 20:25:52 -0500

    reverted to suggestions.length

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -51,7 +51,7 @@ describe("<App /> integration", () => {
     const locations = extractLocations(mockData);
     CitySearchWrapper.setState({ suggestions: locations });
     const suggestions = CitySearchWrapper.state("suggestions");
-    const selectedIndex = Math.floor(Math.random() * (suggestions.length - 1));
+    const selectedIndex = Math.floor(Math.random() * (suggestions.length));
     const selectedCity = suggestions[selectedIndex];
     await CitySearchWrapper.instance().itemClickHandler(selectedCity);
     const allEvents = await getEvents();
