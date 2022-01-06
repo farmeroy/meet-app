72a5ecbcb2d085817acb85c5731f92a3055b545e
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-06 10:52:31 -0500

    added nprogress, conditional api call for localhost vs online, getEvents logic with call to lambda server

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 /**
  *
- * 
+ *
  * @param {*} events:
  * The following function should be in the 'api.js' file.
  * This function takes an events array, then uses map to create a new array with only locations.
@@ -8,8 +8,9 @@
  * The Set will remove all duplicates from the array
  */
 
-import { mockData } from './mock-data';
-import axios from 'axios';
+import { mockData } from "./mock-data";
+import axios from "axios";
+import NProgress from 'nprogress';
 
 export const extractLocations = (events) => {
   var extractLocations = events.map((event) => event.location);
@@ -28,24 +29,43 @@ const checkToken = async (accessToken) => {
 };
 
 export const getEvents = async () => {
-  return mockData;
+  NProgress.start();
+  if (window.location.href.startsWith("http://localhost")) {
+    NProgress.done();
+    return mockData;
+  }
+  const token = await getAccessToken();
+  if (token) {
+    removeQuery();
+    const url = 'https://fw4pf3ipga.execute-api.eu-central-1.amazonaws.com/dev/api/get-events/' + token;
+    const result = await axios.get(url);
+    if (result.data) {
+      var locations = extractLocations(result.data.events);
+      localStorage.setItem('lastEvents', JSON.stringify(result.data));
+      localStorage.setItem('locations', JSON.stringify(locations));
+    }
+    NProgress.done();
+    return result.data.events;
+
+  }
+  
 };
 
 export const getAccessToken = async () => {
-  const accessToken = localStorage.getItem('access_token');
+  const accessToken = localStorage.getItem("access_token");
   const tokenCheck = accessToken && (await checkToken(accessToken));
   if (!accessToken || tokenCheck.error) {
-    await localStorage.removeItem('access_token');
+    await localStorage.removeItem("access_token");
     const searchParams = new URLSearchParams(window.location.search);
-    const code = await searchParams.get('code');
+    const code = await searchParams.get("code");
     if (!code) {
       const results = await axios.get(
-      "https://fw4pf3ipga.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
+        "https://fw4pf3ipga.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
       );
-      const {authURL} = results.data;
+      const { authURL } = results.data;
       return (window.location.href = authURL);
     }
     return code && getToken(code);
   }
   return accessToken;
-}
+};
