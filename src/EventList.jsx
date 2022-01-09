b521bc76389e709bda2217421081a6d1f47c8317
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-09 03:03:27 +0100

    refactored events.prop in attempt to ensure component updates when props updates, no success

--- src/EventList.jsx
+++ src/EventList.jsx
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 import Event from "./Event";
 
 class EventList extends Component {
+
   render() {
-    const { events } = this.props;
     return (
       <ul className="EventList">
-        {events.map((event) => (
+        {this.props.events.map((event) => (
           <li key={event.id}>
             <Event event={event} />
           </li>
