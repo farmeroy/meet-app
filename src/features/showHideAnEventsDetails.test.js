90e2b171c092e5d6af4c0146fbc6831b1d275f78
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-11 09:29:55 +0100

    added first scenario test

--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -1,49 +1,50 @@
 import { loadFeature, defineFeature } from "jest-cucumber";
+import { mount, shallow } from "enzyme";
+import { mockData } from "../api";
+import App from "../App";
+import EventList from "../EventList";
 
 const feature = loadFeature("./src/features/showHideAnEventsDetails.feature");
 
-defineFeature(feature, test => {
-test('When the user opens the page, all event details are collapsed by default', ({ given, when, then }) => {
-    	given('that the user hasn\'t interacted with the page', () => {
-
-    	});
-
-    	when('the user views the page', () => {
-
-    	});
-
-    	then('all event elements are collapsed', () => {
-
-    	});
+defineFeature(feature, (test) => {
+  test("When the user opens the page, all event details are collapsed by default", ({
+    given,
+    when,
+    then,
+  }) => {
+    given("that the user hasn't interacted with the page", () => {});
+    let AppWrapper;
+    when("the user views the page", () => {
+      AppWrapper = mount(<App />);
     });
 
-    test('The user can click on an event to see its details', ({ given, when, then }) => {
-    	given('that the user is interested in an event', () => {
-
-    	});
-
-    	when('the user clicks on the event', () => {
-
-    	});
-
-    	then('the event element expands and shows its details', () => {
-
-    	});
+    then("all event elements are collapsed", () => {
+      AppWrapper.update();
+      expect(AppWrapper.find('.eventDetails')).toHaveLength(0);
     });
+  });
 
-    test('The user can close an event\'s details by clicking on it', ({ given, when, then }) => {
-    	given('that the event element is expanded', () => {
+  test("The user can click on an event to see its details", ({
+    given,
+    when,
+    then,
+  }) => {
+    given("that the user is interested in an event", () => {});
 
-    	});
+    when("the user clicks on the event", () => {});
 
-    	when('the user is finished reading the details', () => {
+    then("the event element expands and shows its details", () => {});
+  });
 
-    	});
+  test("The user can close an event's details by clicking on it", ({
+    given,
+    when,
+    then,
+  }) => {
+    given("that the event element is expanded", () => {});
 
-    	then('the user can click on the event to collapse it', () => {
+    when("the user is finished reading the details", () => {});
 
-    	});
-    });
-
+    then("the user can click on the event to collapse it", () => {});
+  });
 });
-
