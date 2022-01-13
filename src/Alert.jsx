62a0d986f80a0c3828d5e7d430a0741ca09ea01a
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-13 11:14:28 +0100

    added error alert, updated className => "alert"

--- src/Alert.jsx
+++ src/Alert.jsx
@@ -14,7 +14,7 @@ class Alert extends Component {
 
   render() {
     return (
-      <div className="Alert">
+      <div className="alert">
         <p style={this.getStyle()}>{this.props.text}</p>
       </div>
     );
@@ -28,4 +28,11 @@ class InfoAlert extends Alert {
   }
 }
 
-export { InfoAlert };
+class ErrorAlert extends Alert {
+   constructor(props) {
+    super(props);
+    this.color = "red";
+  }
+}
+
+export { InfoAlert, ErrorAlert };
