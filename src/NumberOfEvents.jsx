1b1c2a6891f2dc7f9edfce61a6601c69cc7846f2
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-13 13:29:20 +0100

    added ErrorAlert

--- src/NumberOfEvents.jsx
+++ src/NumberOfEvents.jsx
@@ -1,26 +1,53 @@
 import React, { Component } from "react";
+import { ErrorAlert } from "./Alert";
 
 class NumberOfEvents extends Component {
   state = {
     numberInputValue: 32,
   };
 
-
   onChangeHandler = (event) => {
-    this.setState({
-      numberInputValue: event.target.value,
-    });
-    this.props.updateNumber(event.target.value);
+    let num = event.target.value;
+    if (num.length === 0) {
+      num = 1;
+    }
+    if (num < 0) {
+      this.setState({
+        numberInputValue: 0,
+        infoText: "Please enter a number greater than zero",
+      });
+      this.props.updateNumber(0);
+    } else if (num > 64) {
+      this.setState({
+        numberInputValue: 64,
+        infoText: "This app will display maximum 64 events",
+      });
+      this.props.updateNumber(64);
+    } else {
+      this.setState({
+        numberInputValue: num,
+        infoText: ""
+      });
+      this.props.updateNumber(num);
+    }
   };
 
   render() {
     return (
-      <input
-        className="numberInput"
-        type="number"
-        value={this.state.numberInputValue}
-        onChange={(event) => this.onChangeHandler(event)}
-      />
+      <>
+      <label for="number-input">
+        Events:
+      </label>
+        <input
+          className="numberInput"
+          id="number-input"
+          name="number-input"
+          type="number"
+          value={this.state.numberInputValue}
+          onChange={(event) => this.onChangeHandler(event)}
+        />
+      <ErrorAlert text={this.state.infoText} />
+      </>
     );
   }
 }
