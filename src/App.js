831fa2380af0ed76f2f494de91073dfa38cbbdb8
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-17 20:16:05 +0100

    implement <WelcomeScreen>

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import "./nprogress.css";
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
 import NumberOfEvents from "./NumberOfEvents";
-import { InfoAlert } from './Alert';
-import { getEvents, extractLocations } from "./api";
+import WelcomeScreen from "./WelcomeScreen";
+import { InfoAlert } from "./Alert";
+import { getEvents, extractLocations, getAccessToken } from "./api";
 
 class App extends Component {
   state = {
@@ -13,18 +14,31 @@ class App extends Component {
     locations: [],
     eventsNumber: 16,
     selectedLocation: "",
-    isOnline: true
+    isOnline: true,
+    showWelcomeScreen: undefined,
   };
 
   componentDidMount() {
     this.mounted = true;
- if (!navigator.onLine) {
+    const accessToken = localStorage.getItem("access_token");
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if (code || (isTokenValid && this.mounted)) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ events, locations: extractLocations(events) });
+        }
+      });
+    }
+    if (!navigator.onLine) {
       this.setState({
-        isOnline: false
-      })
+        isOnline: false,
+      });
     }
 
-       getEvents().then((events) => {
+    getEvents().then((events) => {
       if (this.mounted) {
         this.setState({ events, locations: extractLocations(events) });
       }
@@ -36,12 +50,11 @@ class App extends Component {
   }
 
   updateEvents = (location, eventsNumber = this.state.eventsNumber) => {
-    this.setState({isOnline: navigator.onLine ? true : false})
+    this.setState({ isOnline: navigator.onLine ? true : false });
     getEvents().then((events) => {
-      const locationEvents =
-        !location
-          ? events
-          : events.filter((event) => event.location === location);
+      const locationEvents = !location
+        ? events
+        : events.filter((event) => event.location === location);
       this.setState({
         events: locationEvents.slice(0, eventsNumber),
         location: location,
@@ -58,17 +71,30 @@ class App extends Component {
   };
 
   render() {
+    if (this.state.showWelcomeScreen === undefined) {
+      return <div className="App" />;
+    }
     return (
       <div className="App">
         <h1 className="title">Meet</h1>
-        <p>Find newbie developer events in your city and meet other learners!</p>
-      {!this.state.isOnline && <InfoAlert text="You are offline. This event list is loaded from your cache" />}
+        <p>
+          Find newbie developer events in your city and meet other learners!
+        </p>
+        {!this.state.isOnline && (
+          <InfoAlert text="You are offline. This event list is loaded from your cache" />
+        )}
         <CitySearch
           locations={this.state.locations}
           updateEvents={this.updateEvents}
         />
         <NumberOfEvents updateNumber={this.updateEventsNumber} />
         <EventList events={this.state.events} />
+        <WelcomeScreen
+          showWelcomeScreen={this.state.showWelcomeScreen}
+          getAccessToken={() => {
+            getAccessToken();
+          }}
+        />
       </div>
     );
   }
